1d48d65a836b4c2b04d4e31f251c997a0762d237
mrpurrfect <103968215+mrpurrfect@users.noreply.github.com>
2022-07-20 12:37:21 -0400

    main menu, item held added

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
+enum ActionKind {
+    Walking,
+    Idle,
+    Jumping,
+    cursor_none,
+    cursor_tilledsoil,
+    cursor_seed1,
+    cursor_seed2,
+    cursor_seed3,
+    cursor_seed4
+}
+let heldnum = 0
+let heldstring = ""
+let LoadRow = 0
+let LoadCol = 0
+let menu = 0
 scene.setBackgroundImage(img`
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
@@ -120,3 +136,190 @@ scene.setBackgroundImage(img`
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
     9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999
     `)
+tiles.setCurrentTilemap(tileUtil.createSmallMap(tilemap`level1`))
+if (blockSettings.exists("Played Before")) {
+    story.showPlayerChoices("Continue", "New Game")
+} else {
+    story.showPlayerChoices("New Game", "")
+}
+if (story.checkLastAnswer("Continue")) {
+    LoadCol = 1
+    LoadRow = 8
+    for (let index = 0; index < 6; index++) {
+        for (let index = 0; index < 18; index++) {
+            if (blockSettings.readNumber("soil at " + LoadCol + ", " + LoadRow) == 0) {
+                tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile1`)
+            } else if (blockSettings.readNumber("soil at " + LoadCol + ", " + LoadRow) == 1) {
+                tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile2`)
+            } else if (blockSettings.readNumber("soil at " + LoadCol + ", " + LoadRow) == 2) {
+                if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 1) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile3`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 2) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile6`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 3) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile8`)
+                }
+            } else if (blockSettings.readNumber("soil at " + LoadCol + ", " + LoadRow) == 3) {
+                if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 1) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile11`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 2) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile14`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 3) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile15`)
+                }
+            } else if (blockSettings.readNumber("soil at " + LoadCol + ", " + LoadRow) == 4) {
+                if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 1) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile16`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 2) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile19`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 3) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile21`)
+                }
+            } else if (blockSettings.readNumber("soil at " + LoadCol + ", " + LoadRow) == 5) {
+                if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 1) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile23`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 2) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile24`)
+                } else if (blockSettings.readNumber("growth at " + LoadCol + ", " + LoadRow) == 3) {
+                    tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile25`)
+                }
+            }
+            LoadCol += 1
+        }
+        LoadCol = 1
+        LoadRow += 1
+    }
+} else if (story.checkLastAnswer("New Game")) {
+    blockSettings.clear()
+    blockSettings.writeNumber("Played Before", 1)
+    blockSettings.writeNumber("numofhoe", 3)
+    blockSettings.writeNumber("numofseed1", 1)
+    blockSettings.writeNumber("numofseed2", 1)
+    blockSettings.writeNumber("numofseed3", 1)
+    blockSettings.writeNumber("numofseed4", 1)
+    LoadCol = 1
+    LoadRow = 8
+    for (let index = 0; index < 6; index++) {
+        for (let index = 0; index < 18; index++) {
+            blockSettings.writeNumber("soil at " + LoadCol + ", " + LoadRow, 0)
+            tiles.setTileAt(tiles.getTileLocation(LoadCol, LoadRow), assets.tile`myTile1`)
+            LoadCol += 1
+        }
+        LoadCol = 1
+        LoadRow += 1
+    }
+}
+let mySprite = sprites.create(img`
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    `, SpriteKind.Player)
+let anim = animation.createAnimation(ActionKind.cursor_none, 1000)
+anim.addAnimationFrame(img`
+    . . . . . . . . 
+    . . . . . . . . 
+    . . f f f f . . 
+    . . f 1 1 f . . 
+    . . f 1 1 f . . 
+    . . f f f f . . 
+    . . . . . . . . 
+    . . . . . . . . 
+    `)
+animation.attachAnimation(mySprite, anim)
+let anim2 = animation.createAnimation(ActionKind.cursor_tilledsoil, 1000)
+anim2.addAnimationFrame(img`
+    . f f f f f f . 
+    . f e b b b f . 
+    . f e 1 1 b f . 
+    . f e 1 1 1 f . 
+    . f e 1 1 1 f . 
+    . f e 1 1 1 f . 
+    . f e 1 1 1 f . 
+    . f f f f f f . 
+    `)
+animation.attachAnimation(mySprite, anim2)
+let anim3 = animation.createAnimation(ActionKind.cursor_seed1, 1000)
+anim3.addAnimationFrame(img`
+    . f f f f f f . 
+    . f 6 6 6 d f . 
+    . f 6 d 6 5 f . 
+    . f 6 5 6 d f . 
+    . f 6 d 6 5 f . 
+    . f 6 5 6 5 f . 
+    . f 6 5 6 6 f . 
+    . f f f f f f . 
+    `)
+animation.attachAnimation(mySprite, anim3)
+let anim4 = animation.createAnimation(ActionKind.cursor_seed2, 1000)
+anim4.addAnimationFrame(img`
+    . f f f f f f . 
+    . f 6 1 1 6 f . 
+    . f 6 1 1 6 f . 
+    . f 1 1 1 1 f . 
+    . f 1 1 1 1 f . 
+    . f 1 1 1 1 f . 
+    . f 6 1 1 6 f . 
+    . f f f f f f . 
+    `)
+animation.attachAnimation(mySprite, anim4)
+let anim5 = animation.createAnimation(ActionKind.cursor_seed3, 1000)
+anim5.addAnimationFrame(img`
+    . f f f f f f . 
+    . f 6 6 2 4 f . 
+    . f 6 2 4 6 f . 
+    . f 6 2 4 6 f . 
+    . f 6 6 2 4 f . 
+    . f 6 6 2 4 f . 
+    . f 6 2 4 6 f . 
+    . f f f f f f . 
+    `)
+animation.attachAnimation(mySprite, anim5)
+let anim6 = animation.createAnimation(ActionKind.cursor_seed4, 1000)
+anim5.addAnimationFrame(img`
+    . f f f f f f . 
+    . f 6 c c 6 f . 
+    . f c 1 1 c f . 
+    . f c 1 1 c f . 
+    . f b c c b f . 
+    . f b b b b f . 
+    . f 6 b b 6 f . 
+    . f f f f f f . 
+    `)
+animation.attachAnimation(mySprite, anim6)
+animation.setAction(mySprite, ActionKind.cursor_none)
+let textSprite = textsprite.create("Nothing", 15, 1)
+grid.snap(textSprite)
+grid.snap(mySprite)
+grid.moveWithButtons(mySprite)
+let itemheld = 0
+menu = 1
+game.onUpdateInterval(500, function () {
+    if (menu == 1) {
+        grid.place(textSprite, tiles.getTileLocation(mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Top).column, mySprite.tilemapLocation().getNeighboringLocation(CollisionDirection.Top).row))
+        if (itemheld == 0) {
+            heldstring = "Nothing"
+            heldnum = 0
+        } else if (itemheld == 1) {
+            heldstring = "Hoe"
+            heldnum = blockSettings.readNumber("numofhoe")
+        } else if (itemheld == 2) {
+            heldstring = "Wheat"
+            heldnum = blockSettings.readNumber("numofseed1")
+        } else if (itemheld == 3) {
+            heldstring = "Egg"
+            heldnum = blockSettings.readNumber("numofseed2")
+        } else if (itemheld == 4) {
+            heldstring = "Bacon"
+            heldnum = blockSettings.readNumber("numofseed3")
+        } else if (itemheld == 5) {
+            heldstring = "Milk"
+            heldnum = blockSettings.readNumber("numofseed4")
+        }
+        textSprite.setText("" + convertToText(heldnum) + " " + heldstring)
+    }
+})
